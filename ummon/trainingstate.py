ce5e3d786bfc3cd3337a66f6ffb4965b792515de
Matthias Hermann <ubootfenster@googlemail.com>
2018-03-30 11:05:50 +0200

    [Add] trainingstate.py: Persisting State during training

--- ummon/trainingstate.py
+++ ummon/trainingstate.py
@@ -1,40 +1,81 @@
-##########################################################################
-# Append the path to ummon3 to PATH-Variable so that ummon can be imported
+##################################################################################################################
+# Append the path to ummon3 to PATH-Variable so that ummon can be imported during development
 import sys
-sys.path.insert(0,'../../ummon3')  
-sys.path.insert(0,'../ummon3')     
-##########################################################################
-
+sys.path.insert(0,'../../ummon3')  # for python basicusage.py
+sys.path.insert(0,'../ummon3')     # for python examples/basicusage.py
+#############################################################################################
+import torch
+import shutil
 
 class Trainingstate():
+    """
+    Small wrapper class for persisting trainingstate as a Python Dictionary. 
+    It combines `model.state_dict()` and `optimzer.state_dict()` plus some information during training like 
+    the current learning rate and the loss function.
     
-    def __init__():
+    E.g. Trainingstate().state["loss[]"] gives you a list of tuples as [(epoch, loss)] over the whole training.
+    
+    Members
+    -------
+    
+    state : Dictionary
+            Python Dictionary that holdes the training state
+            
+    
+    Methods
+    -------
+    
+    update_state()  : Method for updating training state
+    load_state()    : Method for loading a persisted state
+    save_state()    : Method for persisting the current training state.
+    
+    """
+    
+    def __init__(self):
         
+        self.state = None
+    
+    def update_state(self, model, optimizer, epoch, iteration, loss, accuracy):
+        if self.state is None:
+            self.state = {  
+                             "model_desc" : str(model),
+                             "init_optimizer_state" : optimizer.state_dict(),
+                             "best_loss" : (epoch, loss),
+                             "best_accuracy" : (epoch, accuracy),
+                             "cuda" : next(model.parameters()).is_cuda, 
+                             "loss[]" : [(epoch, loss)],
+                             "lrate[]" : [optimizer.state_dict()["param_groups"][0]["lr"]],
+                             "accuracy[]" : [(epoch, accuracy)],
+                             "model_state" : model.state_dict(),
+                             "optimizer_state": optimizer.state_dict(),
+                          }
+        else:
+             self.state = {  
+                             "model_desc" : str(model),
+                             "init_optimizer_state" : self.state["init_optimizer_state"],
+                             "best_loss" : (epoch, loss) if loss < self.state["best_loss"][1] else self.state["best_loss"],
+                             "best_accuracy" : (epoch, accuracy) if accuracy < self.state["best_accuracy"][1] else self.state["best_accuracy"],
+                             "cuda" : self.state["cuda"], 
+                             "loss[]" : [*self.state["loss[]"], (epoch, loss)],
+                             "lrate[]" : [*self.state["lrate[]"], optimizer.state_dict()["param_groups"][0]["lr"]],
+                             "accuracy[]" : [*self.state["accuracy[]"], (epoch, accuracy)],
+                             "model_state" : model.state_dict(),
+                             "optimizer_state": optimizer.state_dict(),
+                          }
+        
+    def load_state(self, filename, force_cpu = False):
+        if force_cpu:
+            self.state = torch.load(filename, map_location=lambda storage, loc: storage)
+        else:
+            self.state = torch.load(filename)        
+        
+    def save_state(self, filename):
+        torch.save(self.state, filename)
+        
+        def is_best(state):
+            return state["loss"][-1] == state["best_loss"]
+        
+        if is_best(self.state):
+            shutil.copyfile(filename, 'model_best.pth.tar')
 
-
-     training_state = {"args" : args, 
-                         "error[]" : [error],
-                         "classification_loss[]" : [classification_loss.data[0]],
-                         "policy_loss[]": [_policy_loss.data[0]],
-                         "value_loss[]" : [_value_loss.data[0]],
-                         "total_loss[]" : [total_loss],
-                         "best_total_loss" : total_loss,
-                         "best_error" : error,
-                         "avg_scale[]" : [avg_scale],
-                         "avg_locx[]" : [avg_x_loc],
-                         "avg_locy[]" : [avg_y_loc],
-                         "model" : model.state_dict(),}
-    training_state = { "args" : args, 
-                         "error[]" : [*learning_state["error[]"], error], 
-                         "classification_loss[]" : [*learning_state["classification_loss[]"], classification_loss.data[0]],
-                         "policy_loss[]": [*learning_state["policy_loss[]"], _policy_loss.data[0]],
-                         "value_loss[]" : [*learning_state["value_loss[]"], _value_loss.data[0]],
-                         "total_loss[]" : [*learning_state["total_loss[]"], total_loss],
-                         "best_total_loss" : total_loss if total_loss < learning_state["best_total_loss"] else learning_state["best_total_loss"],
-                         "best_error" : error if error < learning_state["best_error"] else learning_state["best_error"],
-                         "avg_scale[]" : [*learning_state["avg_scale[]"], avg_scale],
-                         "avg_locx[]" : [*learning_state["avg_locx[]"], avg_x_loc],
-                         "avg_locy[]" : [*learning_state["avg_locy[]"], avg_y_loc],
-                         "model" : model.state_dict(),}
-
-
+        
