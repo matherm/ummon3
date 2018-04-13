c998af8910b83fec750ba63d6d22f33f53f652c8
Matthias Franz <mfranz@htwg-konstanz.de>
2018-04-13 15:36:51 +0200

    Removed line breaks from logger output (they destroy readability of log file).

--- ummon/logger.py
+++ ummon/logger.py
@@ -222,7 +222,8 @@ class Logger(logging.getLoggerClass()):
             learningstate.state["best_validation_loss"][1]
         detailed_loss = learningstate.state["detailed_loss[]"][-1][1]
         
-        self.info('\nModel Evaluation, Epoch #{}, lrate {}'.format(epoch, lrate))
+        self.info(' ')
+        self.info('Model Evaluation, Epoch #{}, lrate {}'.format(epoch, lrate))
         self.info("----------------------------------------")  
         if regression == True:
             self.info('       Validation set: loss: {:.4f}. {}'.format(loss, 
@@ -231,6 +232,7 @@ class Logger(logging.getLoggerClass()):
             self.info('       Validation set: loss: {:.4f}, Accuracy: {}/{} ({:.2f}%), Error: {:.2f}%. {}'.format(
                 loss, int(acc * batchsize), batchsize, acc * 100, (1. - acc) * 100, 
                 "[BEST]" if is_best else ''))
+        detailed_loss = detailed_loss.replace('\n', ' ').replace('\r', ' ')
         self.info('       Detailed loss information: {}'.format(detailed_loss))
         self.info('       Throughput is {:.0f} samples/s'.format(samples_per_seconds))
         if profile:
