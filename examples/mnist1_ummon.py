[Fix] numerical issues with mnist1_example

examples/mnist1_ummon.py
@@ -40,16 +40,16 @@ cnet = Sequential(
     ('line1', Linear( [30],        10, 'xavier_normal'))
 )
 
-# loss
-loss = nn.BCEWithLogitsLoss()
+# loss (size_averaging is numerically unstable)
+loss = nn.BCEWithLogitsLoss(size_average = False)
 
 # optimizer
-opt = torch.optim.SGD(cnet.parameters(), lr=0.1)
+opt = torch.optim.SGD(cnet.parameters(), lr=0.1 / 16)
 
 with Logger(logdir='.', log_batch_interval=5000) as lg:
     
     # trainer
-    trn = ClassificationTrainer(lg, cnet, loss, opt, precision=np.float32)
+    trn = ClassificationTrainer(lg, cnet, loss, opt)
     
     # train
     trn.fit((x0,y0,16), epochs=35, validation_set=(x2,y2), eval_interval=5)