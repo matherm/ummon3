f16d7e09a0d051dfc9012cb1cb1a2fc152cf5918
Matthias Hermann <Matthias.Hermann@htwg-konstanz.de>
2018-06-19 17:12:15 +0200

    Fixes white anomalies

--- ummon/preprocessing/anomaly.py
+++ ummon/preprocessing/anomaly.py
@@ -7,6 +7,7 @@ Created on Fri Jun 15 09:13:39 2018
 """
 
 import numpy as np
+import torch
 class SquareAnomaly():
     """
     Adds a random square anomaly to a given patch.
@@ -29,17 +30,30 @@ class SquareAnomaly():
     """
     def __init__(self, size=16, color=0):
          self.anom_size = size
+         self.color = color
     
     def __call__(self, x):
         assert np.max(x.numpy()) <= 1 and np.min(x.numpy()) >= 0
         assert x.size(0) < x.size(1) and x.size(0) < x.size(2)
+        if x.dtype == torch.float and x.max() > 1.:
+            # Data is float and we have a range greater than 1
+            if self.color <= 1:
+                # color is defined in range [0, 1]
+                self.color = self.color * 255
+        
+        if x.dtype == torch.float and x.min() >= 0 and x.max() <= 1.:
+            # Data is float and we have a range between [0, 1]
+            if self.color > 1:
+                # color is defined in range [0, 255]
+                self.color = self.color / 255
+            
         if x.dim() == 3:
             _y = np.random.randint(0, x.size(1) - self.anom_size)
             _x = np.random.randint(0, x.size(2) - self.anom_size)
-            x[:, _y : _y + self.anom_size, _x : _x + self.anom_size] = 0
+            x[:, _y : _y + self.anom_size, _x : _x + self.anom_size] = self.color
         else:
             _y = np.random.randint(0, x.size(0) - self.anom_size)
             _x = np.random.randint(0, x.size(1) - self.anom_size)
-            x[_y : _y + self.anom_size, _x : _x + self.anom_size] = 0
+            x[_y : _y + self.anom_size, _x : _x + self.anom_size] = self.color
         return x
             
