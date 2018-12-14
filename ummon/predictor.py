Fixes -1 1 label normalization in predictor.py resulting in confusing results.

ummon/predictor.py
@@ -128,12 +128,6 @@ class Predictor:
             if isinstance(loss_function, torch.nn.CrossEntropyLoss):
                 output = F.softmax(Variable(output), dim=1).data
                 
-        
-        # Case single output neurons (e.g. one-class-svm sign(output))
-        if (output.dim() > 1 and output.size(1) == 1) or output.dim() == 1:
-            # makes zeroes positive
-            classes = (output + 1e-12).sign().long()    
-        
         # One-Hot-Encoding
         if (output.dim() > 1 and output.size(1) > 1):
             classes = output.max(1, keepdim=True)[1] 
@@ -143,14 +137,7 @@ class Predictor:
     @staticmethod
     def compute_accuracy(classes, targets):
         assert targets.shape[0] == classes.shape[0]
-        
-        # Case single output neurons (e.g. one-class-svm sign(output))
-        if (targets.dim() > 1 and targets.size(1) == 1) or targets.dim() == 1:
-            # Sanity check binary case
-            if not targets.max() > 1 and not classes.max() > 1:
-                # Transform 0,1 encoding to -1 +1
-                targets = (targets.float() - 1e-12).sign().long()
-        
+              
         # Classification one-hot coded targets are first converted in class labels
         if targets.dim() > 1 and targets.size(1) > 1:
             targets = targets.max(1, keepdim=True)[1]