3e0a16d474260b78cccc675be28980c9a37a3dcf
Matthias Hermann <ubootfenster@googlemail.com>
2019-01-03 18:48:40 +0100

    cleans up repo

--- _create_conda_env.sh
+++ _create_conda_env.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 conda env create -f environment.yml #$@%  
+#conda env export | grep -v "^prefix: " > environment.yml
+#conda env export > environment.yml
+#conda env update -f environment.yml #$@%  
