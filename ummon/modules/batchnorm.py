6b13f256596d3259530a3a295477629ea1191b9b
Matthias Franz <mfranz@htwg-konstanz.de>
2019-01-09 11:03:41 +1300

    Updated comments in batchNorm1d.

--- ummon/modules/batchnorm.py
+++ ummon/modules/batchnorm.py
@@ -9,25 +9,15 @@ class BatchNorm1d(nn.BatchNorm1d):
     '''
     1 d batch normalization::
     
-        bn10 = BatchNorm1d([n,p,q], kernel_size, stride, padding)
+        bano = BatchNorm1d([n,p], eps, momentum, affine, track_running_stats)
     
-    creates a max pooling layer. This method selects the local maximum in the 
-    neighborhood determined by 'kernel_size' as the output value. The error is sparsely 
-    backpropagated, i.e., only the pixels where the maximum occurred is updated with the 
-    backpropagated error, all others are ignored.
-    
-    Applies max pooling to subsample an input tensor and provides a certain translation 
-    invariance. The input tensor for this layer must be in a non-flattened format. Pooling is 
-    controlled by 3 attributes: the stride between window centers in x- and y-direction
-    ('stride': either a number or 2-tuple), and the pooling window size ('kernel_size': 
-    either a number or 2-tuple). You can set an additional padding region ('padding':
-    either a number or 2-tuple) filled with zeroes for treating the image boundary 
-    regions.
-    
-    You obtain the classical pooling in the valid region (as, e.g., in LeNet) by setting
-    stride and kernel size to the same values and by setting 'padding' to zero. If you
-    want a behaviour similar to a strided convolution with zero padding set 'padding'
-    to half the filter size in each direction. For examples, see the tests in ummon_test.py.
+    creates a 1d batch normalization layer which often improves convergence and learning
+    speed. For the theory, see Ioffe, S. and Szegedy, C. (2015). Batch normalization: 
+    Accelerating deep network training by reducing internal covariate shift. arXiv preprint 
+    arXiv:1502.03167. For the meaning of the parameters, see PyTorch documenation of
+    nn.BatchNorm1d. The input tensor for this layer must be in a non-flattened 1d format,
+    each example consisting of 'p' features and 'n' channels. For examples, see the test 
+    in ummon_test.py.
     '''    
     # constructor
     def __init__(self, insize, eps=1e-05, momentum=0.1, affine=True, 
