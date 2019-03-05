Fixes hash computation in pre transform dataset

tests/ummon_dataset_test.py
@@ -57,7 +57,7 @@ class TestDatasets(unittest.TestCase):
 
     def test_pre_transform_dataset(self):
         transform = transforms.Compose(
-            [transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]), flatten_transform]
+            [transforms.Normalize(mean=[0., 0., 0.], std=[1.0, 1.0, 1.0]), flatten_transform]
             )
         path = "__ummoncache__/test/pretransformed"
         data = torch.from_numpy(np.random.normal(0,1,size=10*10*10*3)).float().view(10, 3,10,10)

ummon/datasets/pre_transform_dataset.py
@@ -26,10 +26,17 @@ class PreTransformDataset(Dataset):
                 raise ValueError("Dataset also specifies transforms. Exiting..")
         
         # choose the right data partition to load
-        self.str_pretransform = hash_obj(pre_transform)
-        self.str_pretransform += hash_obj(pre_transform(dataset[0][0]))
-        self.filename_labels = "{}_labels.pt".format(os.path.join(path, self.str_pretransform))
+        sample = pre_transform(dataset[0][0]) + pre_transform(dataset[len(dataset)-1][0])
+        self.str_pretransform = hash_tensor(sample)
+
+        # check uniqueness
+        #assert self.str_pretransform != hash_tensor(sample[0] + 0.0001)
+        #assert self.str_pretransform != hash_tensor(pre_transform(dataset[1][0]))
+        
         self.filename_data   = "{}_data.pt".format(os.path.join(path, self.str_pretransform))
+        self.filename_labels = "{}_labels.pt".format(os.path.join(path, self.str_pretransform))
+
+        accquire_lock()
 
         if not os.path.exists(self.filename_data) or not os.path.exists(self.filename_labels):
             data, labels = self.process(self.dataset, self.pre_transform, self.pre_filter)
@@ -37,7 +44,11 @@ class PreTransformDataset(Dataset):
             torch.save(data, self.filename_data)
             torch.save(labels, self.filename_labels)
 
+        release_lock()
+
         self.data, self.labels = torch.load(self.filename_data), torch.load(self.filename_labels)
+
+        assert len(self.dataset) == len(self.data)
 
     def __repr__(self):
         return "{}({}, pre_transform={})".format(self.__class__.__name__, repr(self.dataset), str(self.pre_transform).replace("\n", ""))
@@ -46,7 +57,6 @@ class PreTransformDataset(Dataset):
         return self.data[idx], self.labels[idx]
 
     def __len__(self):
-        assert len(self.dataset) == len(self.data)
         return len(self.dataset)
 
     def process(self, dataset, pre_transform, pre_filter):
@@ -97,15 +107,16 @@ class PreTransformDataset(Dataset):
                         continue
 
                 if self.pre_transform is not None:
-                    item = self.pre_transform(item[0])       
-                    if not torch.is_tensor(item[0]):
-                        data_list.append(torch.Tensor(item[0]))
+                    item_data = self.pre_transform(item[0])       
+                    item_label = item[1]
+                    if not torch.is_tensor(item_data):
+                        data_list.append(torch.Tensor(item_data))
                     else:
-                        data_list.append(item[0])
-                    if not torch.is_tensor(item[1]):
-                        label_list.append(torch.Tensor(item[1]))
+                        data_list.append(item_data)
+                    if not torch.is_tensor(item_label):
+                        label_list.append(torch.Tensor(item_label))
                     else:
-                        label_list.append(item[1])
+                        label_list.append(item_label)
 
         if isinstance(ds, DataLoader):
             torch.multiprocessing.set_sharing_strategy('file_descriptor')
@@ -128,6 +139,10 @@ def hash_obj(obj):
 def hash(obj):
     return hashlib.md5(obj).hexdigest()
 
+def hash_tensor(obj):
+    if isinstance(obj, np.ndarray):
+        return hash(obj.tobytes())
+    return hash(obj.numpy().tobytes())
 
 def accquire_lock(LOCK = ".pre_processing_lock"):
     while os.path.exists(LOCK):