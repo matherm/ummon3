f585a87e5991f0fd5a0b1df082152108abbc4832
Daniel <ddold@htwg-konstanz.de>
2019-04-11 14:39:18 +0200

    fixed gram diag. added gram diag moudule. added squeezenet features

---
+++ ummon/features/squeezenet_features.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+# @Author: Daniel
+# @Date:   2019-04-10 14:30:56
+# @Last Modified by:   Daniel
+# @Last Modified time: 2019-04-11 14:36:15
+from torchvision.models.squeezenet import squeezenet1_0, squeezenet1_1
+import torch
+import torch.nn as nn
+import copy
+from ummon.modules.gram import GramMatrix, GramDiag
+
+
+class SqueezeNetFeatures(nn.Module):
+
+    def __init__(self, version=1.0, num_classes=1000, pretrained=False, layer="", cuda=False, gram=False, gram_diag=False, gram_diagonal_squared=False):
+        super().__init__()
+        if version not in [1.0, 1.1]:
+            raise ValueError("Unsupported SqueezeNet version {version}:"
+                             "1.0 or 1.1 expected".format(version=version))
+        self.num_classes = num_classes
+        self.__cuda = cuda
+        # build layer names for selection
+        if version == 1.0:
+            pytorch_squeeze = squeezenet1_0(
+                pretrained, num_classes=num_classes)
+            features_names = [
+                "conv_1",
+                "relu_1",
+                "maxpool_1",
+                "fire_2",
+                "fire_3",
+                "fire_4",
+                "maxpool_4",
+                "fire_5",
+                "fire_6",
+                "fire_7",
+                "fire_8",
+                "maxpool_8",
+                "fire_9"]
+        else:
+            pytorch_squeeze = squeezenet1_1(
+                pretrained, num_classes=num_classes)
+            features_names = [
+                "conv_1",
+                "relu_1",
+                "maxpool_1",
+                "fire_2",
+                "fire_3",
+                "maxpool_3",
+                "fire_4",
+                "fire_5",
+                "maxpool_5",
+                "fire_6",
+                "fire_7",
+                "fire_8",
+                "fire_9"]
+        # Final convolution is initialized differently form the rest
+        classifier_names = [
+            "drop_10",
+            "conv_10",
+            "relu_10",
+            "avgpool_10"]
+
+        # copy selected squeezenet features
+        self.features = torch.nn.Sequential()
+        for name, module in zip(features_names, pytorch_squeeze.features):
+            self.features.add_module(name, copy.deepcopy(module))
+            if layer is name:
+                break
+        # copy the clasifier layer?
+        if len(features_names) == len(self.features) and layer != features_names[-1]:
+            for name, module in zip(classifier_names, pytorch_squeeze.classifier):
+                self.features.add_module(name, copy.deepcopy(module))
+                if layer is name:
+                    break
+        del(pytorch_squeeze)
+
+        # add gram calculation?
+        if gram:
+            self.features.add_module("gram matrix", GramMatrix())
+        elif gram_diag:
+            self.features.add_module(
+                "gram diagonal", GramDiag(gram_diagonal_squared))
+
+        # cuda support
+        if self.__cuda:
+            self = self.cuda()
+
+    def forward(self, x):
+        x = x.cuda() if self.__cuda else x
+        x = self.features(x)
+        return x
+
+
+def test():
+    print(SqueezeNetFeatures(pretrained=True, layer="fire_7"))
+    print(SqueezeNetFeatures(pretrained=True, layer="fire_9"))
+    print(SqueezeNetFeatures(pretrained=True, layer="conv_10"))
+
+
+if __name__ == '__main__':
+    test()
--- ummon/modules/gram.py
+++ ummon/modules/gram.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Created on Thu Jun 14 16:44:12 2018
@@ -6,16 +5,83 @@ Created on Thu Jun 14 16:44:12 2018
 Originally taken from: https://github.com/leongatys/PytorchNeuralStyleTransfer
 
 @author: Fabian Freiberg
+@Last Modified by:   Daniel Dold
+@Last Modified time: 2019-04-11 12:47:07
 """
 import torch.nn as nn
 import torch
 
-__all__ = [ 'GramMatrix']
+__all__ = ['GramMatrix', 'GramDiag']
+
 
 class GramMatrix(nn.Module):
+    ##
+    # @brief      { function_description }
+    ##
+    # @param      self  The object
+    # @param      x     input Tensor shape [batch x in_feature x height x width]
+    ##
+    # @return     gram matrix shape [batch x in_feature x in_feature]
+    ##
     def forward(self, x):
         b, c, h, w = x.size()
         F = x.view(b, c, h * w)
         G = torch.bmm(F, F.transpose(1, 2))
-        G.div_(h*w)
+        G.div_(h * w)
         return G
+
+
+class GramDiag(nn.Module):
+    """
+    docstring for GramDiag
+    """
+
+    ##
+    # @brief      Constructs the object. #
+    #
+    # @param      self                   The object
+    # @param      gram_diagonal_squared  The gram diagonal squared (means
+    #                                    vector^4) #
+    #
+    def __init__(self, gram_diagonal_squared=False):
+        super().__init__()
+        self.__gram_diagonal_squared = gram_diagonal_squared
+
+    ##
+    # @brief      calculate gram diagonal
+    ##
+    # @param      self  The object
+    # @param      x     input tensor shape [batch x in_feature x height x width]
+    ##
+    # @return     gram diagonal shape [batch x in_feature]
+    ##
+    def forward(self, x):
+        b, c, h, w = x.size()
+        x = x.view(b, c, 1, h * w)
+        gram_diag = None
+        for b in range(x.size(0)):
+            if self.__gram_diagonal_squared:
+                z = torch.bmm(x[b] * x[b], (x[b] * x[b]).transpose(2, 1))
+            else:
+                z = torch.bmm(x[b], x[b].transpose(2, 1))
+            if isinstance(gram_diag, torch.Tensor):
+                gram_diag = torch.cat(gram_diag, z)
+            else:
+                gram_diag = z
+        gram_diag = torch.squeeze(gram_diag).unsqueeze(0)
+        return gram_diag.div_(h * w)
+
+
+def test():
+    in_t = torch.linspace(-2, 2, 200).view(1, 8, 5, 5)
+    gram_m = GramMatrix()(in_t)
+    gram_m_d = torch.diagonal(gram_m, dim1=1, dim2=2)
+
+    gram_d = GramDiag()(in_t)
+    print("calculation difference: ", gram_d.data - gram_m_d.data)
+    assert torch.all(1e-6 > torch.abs(gram_d.data -
+                                      gram_m_d.data)), "gram diagonal from gram matrix and gram_diagonal calculation differs"
+
+
+if __name__ == '__main__':
+    test()
