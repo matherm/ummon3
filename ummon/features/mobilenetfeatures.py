64ee2309f70b7989a1298c3d9e75e8d79b3cc804
Daniel <ddold@htwg-konstanz.de>
2019-05-14 10:55:56 +0200

    added mobilenet features

---
+++ ummon/features/mobilenetfeatures.py
@@ -0,0 +1,196 @@
+# -*- coding: utf-8 -*-
+# @Author: Daniel
+# @Date:   2019-04-16 10:37:18
+# @Last Modified by:   Daniel
+# @Last Modified time: 2019-05-14 10:52:05
+import torch
+import torch.nn as nn
+from ummon.modules.mobilenet import MobileNet
+from ummon.quanti.quanti_net import QuantiNetWrapper
+from ummon.quanti.quantization import stdParam, mean_std
+from ummon.modules.gram import GramDiag, GramMatrix
+import logging
+
+
+class MobileNetFeatures():
+    """
+    Extracts features from MobileNet
+
+    Usage
+    ======
+            transform = MobileNet(features="fire_4")
+            transform(tensor)
+
+            OR
+
+            vgg = MobileNetFeatures(features="fire_4")
+            my_transforms = transforms.Compose([transforms.ToTensor(), vgg])
+            test_set = ImagePatches("ummon/datasets/testdata/Wood-0035.png", \
+                                train=False, \
+                                transform=my_transforms)
+
+    Input
+    ======
+        *tensor (torch.Tensor) with shape (B x 3 x min 32 x 32)
+
+    Return
+    =====
+        *feature (torch.Tensor)
+
+    """
+
+    ##
+    # @brief      Constructs the object.
+    ##
+    # @param      self                   The object
+    # @param      features               Selects the last layer to use
+    # @param      cuda                   (bool) using cuda
+    # @param      pretrained             Use pretrained MobileNet
+    # @param      gram                   calculate features using gram matrix
+    # @param      gram_diagonal          calculate features using gram
+    # diagnonal
+    # @param      gram_diagonal_squared  calculate features using gram
+    # diagnonal^4
+    # @param      version                MobileNet version (supported [1])
+    ##
+    def __init__(self,
+                 features="pointwise_conv_8",
+                 cuda=False,
+                 pretrained=True,
+                 gram=False,
+                 gram_diagonal=False,
+                 gram_diagonal_squared=False,
+                 version=1):
+        self.__cuda = cuda
+        self.model = MobileNet(layer=features,
+                               pretrained=pretrained,
+                               gram=gram,
+                               gram_diag=gram_diagonal,
+                               gram_diagonal_squared=gram_diagonal_squared,
+                               version=version)
+
+        if cuda and torch.cuda.is_available():
+            self.model = self.model.cuda()
+        self.model = self.model.eval()
+
+    def __call__(self, x):
+        if x.dim != 3 and x.size(0) != 3:
+            raise ValueError(
+                "Shape of input should be (3, x, x), is:", x.shape)
+        x = x.unsqueeze(0)
+
+        if self.__cuda and torch.cuda.is_available():
+            x = x.cuda()
+
+        feature = self.model(x)
+        feature = feature.detach().cpu().view(-1).contiguous()
+        return feature
+
+
+class MobileNetFeaturesQuanti():
+    """
+    Extracts features from MobileNet
+
+    Usage
+    ======
+            transform = MobileNet(features="fire_4")
+            transform(tensor)
+
+            OR
+
+            vgg = MobileNetFeatures(features="fire_4")
+            my_transforms = transforms.Compose([transforms.ToTensor(), vgg])
+            test_set = ImagePatches("ummon/datasets/testdata/Wood-0035.png", \
+                                train=False, \
+                                transform=my_transforms)
+
+    Input
+    ======
+        *tensor (torch.Tensor) with shape (B x 3 x min 32 x 32)
+
+    Return
+    =====
+        *feature (torch.Tensor)
+
+    """
+
+    ##
+    # @brief      Constructs the object. #
+    #
+    # @param      self                   The object
+    # @param      features               Selects the last layer to use
+    # @param      cuda                   (bool) using cuda
+    # @param      pretrained             Use pretrained MobileNet
+    # @param      gram                   calculate features using gram matrix
+    # @param      gram_diagonal          calculate features using gram diagnonal
+    # @param      gram_diagonal_squared  calculate features using gram
+    #                                    diagnonal^4
+    # @param      version                MobileNet version (supported [1.0,
+    #                                    1.1]) #
+    # @param      bits                   bitlength for quantization
+    # @param      nb_sigma               The number of sigma for distribution
+    #                                    shifting
+    #
+    def __init__(self,
+                 features="pointwise_conv_8",
+                 cuda=False,
+                 pretrained=True,
+                 gram=False,
+                 gram_diagonal=False,
+                 gram_diagonal_squared=False,
+                 version=1,
+                 bits=8,
+                 nb_sigma=1):
+        self.__cuda = cuda
+        self.model = MobileNet(layer=features,
+                               pretrained=pretrained,
+                               gram=gram,
+                               gram_diag=gram_diagonal,
+                               gram_diagonal_squared=gram_diagonal_squared,
+                               version=version)
+
+        if cuda and torch.cuda.is_available():
+            self.model = self.model.cuda()
+        self.model = self.model.eval()
+
+        dist = 1 / 2**(bits - 1)
+        _max = 1 - (dist * 1)
+        _min = -1 + (dist * 1)
+        clip_range = dict(min=_min, max=_max)
+        clip_range_n = dict(min=-1, max=_max)
+        mean, std = mean_std(self.model.parameters())
+        logging.getLogger().info("Parameters mean: {}, parameters std: {}".format(mean, std))
+        params = {
+            "qp_net": stdParam(bits,
+                               divisor=1,
+                               handle_overflow=False,
+                               rounding=False,
+                               clip_range=clip_range_n),
+            "qp_parameters": stdParam(bits,
+                                      divisor=nb_sigma,
+                                      mean=mean,
+                                      std=std,
+                                      handle_overflow=False,
+                                      clip_range=clip_range)}
+        self.model = QuantiNetWrapper(
+            self.model,
+            # apply activation quantization after the following modules
+            modules_for_quanti=[nn.Conv2d,
+                                nn.Linear,
+                                GramMatrix,
+                                GramDiag,
+                                nn.BatchNorm2d])
+        self.model.add_and_perform_quantization(**params)
+
+    def __call__(self, x):
+        if x.dim != 3 and x.size(0) != 3:
+            raise ValueError(
+                "Shape of input should be (3, x, x), is:", x.shape)
+        x = x.unsqueeze(0)
+
+        if self.__cuda and torch.cuda.is_available():
+            x = x.cuda()
+
+        feature = self.model(x)
+        feature = feature.detach().cpu().view(-1).contiguous()
+        return feature
---
+++ ummon/modules/mobilenet.py
@@ -0,0 +1,183 @@
+# -*- coding: utf-8 -*-
+# @Author: Daniel
+# @Date:   2019-05-13 16:10:48
+# @Last Modified by:   Daniel
+# @Last Modified time: 2019-05-14 10:11:24
+
+import torch
+import torch.nn as nn
+from collections import OrderedDict
+from ummon.modules.gram import GramMatrix, GramDiag
+import copy
+import os.path as path
+
+# implementation from https://github.com/marvis/pytorch-mobilenet
+
+
+class MobileNet(nn.Module):
+
+    def __init__(self,
+                 version=1,
+                 num_classes=1000,
+                 pretrained=False,
+                 layer="",
+                 gram=False,
+                 gram_diag=False,
+                 gram_diagonal_squared=False,
+                 params_file=path.dirname(path.abspath(__file__)) +
+                 "/pytorch_mobilenet_params/mobilenet_sgd_rmsprop_69.526.tar"):
+        super().__init__()
+        if version not in [1]:
+            raise ValueError("Unsupported MobileNet version {version}:"
+                             "1 expected".format(version=version))
+
+        self.num_classes = num_classes
+        # build layer names for selection
+        ref_mobile_net = None
+        if version == 1:
+            ref_mobile_net = marvisMobileNet(pretrained,
+                                             params_file=params_file,
+                                             num_classes=num_classes)
+            features_names = {
+                "conv": 5,
+
+                "depthwise_conv_1": 5 + 4,
+                "pointwise_conv_1": 5 + 7,
+
+                "depthwise_conv_2": 5 + 1 * 7 + 4,
+                "pointwise_conv_2": 5 + 1 * 7 + 7,
+
+                "depthwise_conv_3": 5 + 2 * 7 + 4,
+                "pointwise_conv_3": 5 + 2 * 7 + 7,
+
+                "depthwise_conv_4": 5 + 3 * 7 + 4,
+                "pointwise_conv_4": 5 + 3 * 7 + 7,
+
+                "depthwise_conv_5": 5 + 4 * 7 + 4,
+                "pointwise_conv_5": 5 + 4 * 7 + 7,
+
+                "depthwise_conv_6": 5 + 5 * 7 + 4,
+                "pointwise_conv_6": 5 + 5 * 7 + 7,
+
+                "depthwise_conv_7": 5 + 6 * 7 + 4,
+                "pointwise_conv_7": 5 + 6 * 7 + 7,
+
+                "depthwise_conv_8": 5 + 7 * 7 + 4,
+                "pointwise_conv_8": 5 + 7 * 7 + 7,
+
+                "depthwise_conv_9": 5 + 8 * 7 + 4,
+                "pointwise_conv_9": 5 + 8 * 7 + 7,
+
+                "depthwise_conv_10": 5 + 9 * 7 + 4,
+                "pointwise_conv_10": 5 + 9 * 7 + 7,
+
+                "depthwise_conv_11": 5 + 10 * 7 + 4,
+                "pointwise_conv_11": 5 + 10 * 7 + 7,
+
+                "depthwise_conv_12": 5 + 11 * 7 + 4,
+                "pointwise_conv_12": 5 + 11 * 7 + 7,
+
+                "depthwise_conv_13": 5 + 12 * 7 + 4,
+                "pointwise_conv_13": 5 + 12 * 7 + 7,
+
+                "avg_pool": 5 + 13 * 7 + 1,
+
+                "fc": 5 + 13 * 7 + 2
+            }
+
+        assert layer in features_names.keys(),\
+            "layername not supported. Supported names: {}".format(
+                features_names.keys())
+        # copy selected squeezenet features
+        self.features = torch.nn.Sequential()
+
+        for i, (name, module) in enumerate(ref_mobile_net.named_modules()):
+            if type(module) is not nn.Sequential and \
+                    type(module) is not marvisMobileNet:
+                self.features.add_module(str(i) + '_' + name.replace('.', '_'),
+                                         copy.deepcopy(module))
+            if i == features_names[layer]:
+                break
+
+        del ref_mobile_net
+        # add gram calculation?
+        if gram:
+            self.features.add_module("gram matrix", GramMatrix())
+        elif gram_diag:
+            self.features.add_module(
+                "gram diagonal", GramDiag(gram_diagonal_squared))
+
+    def forward(self, x):
+        return self.features(x)
+
+
+class marvisMobileNet(nn.Module):
+    def __init__(self, pretrained=False, params_file="", num_classes=1000):
+        super().__init__()
+        self.pretrained = pretrained
+
+        def conv_bn(inp, oup, stride):
+            return nn.Sequential(
+                nn.Conv2d(inp, oup, 3, stride, 1, bias=False),
+                nn.BatchNorm2d(oup),
+                nn.ReLU(inplace=True)
+            )
+
+        def conv_dw(inp, oup, stride):
+            return nn.Sequential(
+                nn.Conv2d(inp, inp, 3, stride, 1, groups=inp, bias=False),
+                nn.BatchNorm2d(inp),
+                nn.ReLU(inplace=True),
+
+                nn.Conv2d(inp, oup, 1, 1, 0, bias=False),
+                nn.BatchNorm2d(oup),
+                nn.ReLU(inplace=True),
+            )
+
+        self.model = nn.Sequential(
+            conv_bn(3, 32, 2),
+            conv_dw(32, 64, 1),
+            conv_dw(64, 128, 2),
+            conv_dw(128, 128, 1),
+            conv_dw(128, 256, 2),
+            conv_dw(256, 256, 1),
+            conv_dw(256, 512, 2),
+            conv_dw(512, 512, 1),
+            conv_dw(512, 512, 1),
+            conv_dw(512, 512, 1),
+            conv_dw(512, 512, 1),
+            conv_dw(512, 512, 1),
+            conv_dw(512, 1024, 2),
+            conv_dw(1024, 1024, 1),
+            nn.AvgPool2d(7),
+        )
+        self.fc = nn.Linear(1024, num_classes)
+
+        if self.pretrained:
+            # w = torch.load("pytorch-mobilenet/mobilenet_sgd_rmsprop_69.526.tar")
+            # self.load_state_dict(w)
+
+            # https://github.com/marvis/pytorch-mobilenet/issues/14
+            tar = torch.load(params_file)
+            state_dict = tar['state_dict']
+            new_state_dict = OrderedDict()
+            for k, v in state_dict.items():
+                name = k[7:]  # remove `module.`
+                new_state_dict[name] = v
+            self.load_state_dict(new_state_dict)
+
+    def forward(self, x):
+        x = self.model(x)
+        x = x.view(-1, 1024)
+        x = self.fc(x)
+        return x
+
+
+def main():
+    model = MobileNet(pretrained=True, layer="pointwise_conv_2")
+    print(model)
+    # print(marvisMobileNet(pretrained=True))
+
+
+if __name__ == '__main__':
+    main()
