7e12c9b635ff8c731b76c154b1d91cdda6f90689
Henning Krause <hekrause@htwg-konstanz.de>
2019-12-03 13:16:41 +0100

    Changed the torchvision version check.

--- tests/system_test.py
+++ tests/system_test.py
@@ -3,29 +3,36 @@ import unittest
 import torch
 import numpy as np
 import torchvision
+import re
 
 class TestSystem(unittest.TestCase):
 
     def test_framework_system_environment(self):
-        
+
         developer_tested_pytorch_versions = ["0.4.0", "0.4.1", "1.0.0", "1.2.0"]
         assert torch.__version__ in developer_tested_pytorch_versions
-        
+
         developer_tested_numpy_versions =  ["1.13.3", "1.14.0", "1.14.3", "1.15.1", "1.16.3"]
         assert np.version.version in developer_tested_numpy_versions
 
-        developer_tested_torchvision_versions =  ['0.2.1', "0.4.0", '0.4.0a0+9232c4a']
-        assert torchvision.__version__ in developer_tested_torchvision_versions
-        
+        developer_tested_torchvision_versions =  ['0.2.1', "0.4.0"]
+
+        split_point = torchvision.__version__.split('.')
+        split_regex = re.split(r'[a-z]', split_point[2])
+        major, minor, rev = split_point[0], split_point[1], split_regex[0]
+        version = "{}.{}.{}".format(major, minor, rev)
+
+        assert version in developer_tested_torchvision_versions
+
     def test_cuda_system_environment(self):
 
         if not torch.cuda.is_available():
             print('\nWarning: cannot run this test - Cuda is not enabled on your machine.')
-        return    
-    
+        return
+
         developer_tested_cuda_versions = ["8.0.61" , "9.2.148"]
         assert torch.version.cuda in developer_tested_cuda_versions
-        
+
         developer_tested_cudnn_versions = [7005,7102, 7602]
         assert torch.backends.cudnn.version() in developer_tested_cudnn_versions
 
