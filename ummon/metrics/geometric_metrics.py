f74279c0e7c4c03f08e01604ca41e207a262219f
Markus Käppeler <markus.kaeppeler@htwg-konstanz.de>
2019-12-06 14:13:27 +0100

    add find_correspondences and binary AP from sklearn to geometric metrics

--- ummon/metrics/geometric_metrics.py
+++ ummon/metrics/geometric_metrics.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-# @Author: Daniel Dold
+# @Author: Daniel Dold, Markus Käppeler
 # @Date:   2019-11-20 10:08:49
-# @Last Modified by:   Daniel Dold
-# @Last Modified time: 2019-11-20 17:15:55
+# @Last Modified by:   Markus Käppeler
+# @Last Modified time: 2019-12-06 14:12:55
 import numpy as np
 from scipy.spatial.transform import Rotation
 from scipy.sparse import csr_matrix
@@ -10,6 +10,7 @@ from scipy.spatial import HalfspaceIntersection, ConvexHull
 from scipy.spatial.qhull import QhullError
 import logging
 import os
+import sklearn.metrics
 from .base import *
 
 __all__ = ['IoU', 'MeanDistanceError']
@@ -94,6 +95,62 @@ def intersection(cuboid1: dict, cuboid2: dict) -> float:
     return hull.volume
 
 
+def iou(cuboid1: dict, cuboid2: dict) -> float:
+    intersect = intersection(cuboid1, cuboid2)
+    union = np.prod(cuboid1['d']) + np.prod(cuboid2['d']) - intersect
+    return intersect / union
+
+
+def find_correspondences(outputs: list, targets: list, threshold: float) -> np.ndarray:
+    """
+    Computes the y_pred/y_true matrix by finding the correct target for each output. Each output counts to it's
+    largest overlapping target. Multiple detections of the same object are considered as a false detection.
+    To count as a correct prediction (True, True) the iou between an output and a target has to exceed
+    the given threshold.
+    If a target has not matching output, it count as a FN (False, True).
+    If a output has not matching target, it counts as an FP (True, False).
+
+    Args:
+        outputs (list): list of predicted cuboids
+        targets (list): list of target cuboids
+    Returns:
+        np.ndarray (N, 2): array with y_pred(N, 0) and y_true(N, 1), either True or False.
+    """
+    y_pred_true = []
+
+    num_target = len(targets)
+    num_output = len(outputs)
+
+    # calculate all ious
+    ious = np.zeros((num_target, num_output))
+    for i_target, cuboid_target in enumerate(targets):
+        for i_output, cuboid_output in enumerate(outputs):
+            ious[i_target, i_output] = iou(cuboid_target, cuboid_output)
+
+    targets_found = set()  # indices of found targets
+    output_found = set()  # indices of found predictions
+    iou_argsort_target = np.argsort(ious, axis=0)  # sort that each output counts to it's largest overlap target
+    for target_i in reversed(range(num_output)):  # iterate reverse to start with largest iou (from argsort)
+        for pred_i in range(num_target):
+            target_max_i = iou_argsort_target[target_i, pred_i]
+
+            # to count as correct detection area of overlap must exceed the threshold
+            if ious[target_max_i, pred_i] > threshold and pred_i not in output_found:
+                targets_found.add(target_max_i)
+                output_found.add(i_output)
+                y_pred_true += [[1, 1]]  # tp
+                break
+
+    targets_i = set(range(num_output))
+    output_i = set(range(num_target))
+    targets_not_matching = targets_i - targets_found  # targets with no matching detection
+    y_pred_true += [[0, 1] for _ in range(len(targets_not_matching))]  # fn
+    output_not_matching = output_i - output_found  # predictions with not matching targets
+    y_pred_true += [[1, 0] for _ in range(len(output_not_matching))]  # fp
+
+    return np.array(y_pred_true, dtype=bool)
+
+
 class GeometricMetrics(OnlineMetric):
     def __call__(self, output: list, target: list):
         results = [self.func(c1, c2) for c1, c2 in zip(output, target)]
@@ -102,6 +159,29 @@ class GeometricMetrics(OnlineMetric):
     @classmethod
     def __repr__(cls):
         return cls.__name__
+
+
+class BinaryAPSklearn(GeometricMetrics):
+    """
+    Calculates the AP score with the function from Sklearn.
+    At the moment it works only for binary detection without confidence scores.
+    Usage:  ap = BinaryAPSklearn()
+            ap(output_batch: list, target_batch: list) # list of list with cuboids
+    Cuboid parameters (dict):Format:'c' -> center of cuboid array[x,y, ... n]
+                                    'd' -> dimension of cuboid array[length,width, ... n]
+                                    'r' -> rotation of cuboid as 3x3 rot matrix
+    Attributes:
+        func (TYPE): function used in parent class
+    """
+
+    def __init__(self):
+        self.func = self.average_precision_sklearn
+
+    @staticmethod
+    def average_precision_sklearn(output: list, target: list):
+        y_pred_true = find_correspondences(output, target, 0.5)
+        y_pred, y_true = y_pred_true[:, 0], y_pred_true[:, 1]
+        return sklearn.metrics.average_precision_score(y_true, y_pred)
 
 
 class IoU(GeometricMetrics):
@@ -118,11 +198,10 @@ class IoU(GeometricMetrics):
     def __init__(self):
         self.func = self.intersection_over_union
 
+
     @staticmethod
     def intersection_over_union(output: dict, target: dict):
-        intersect = intersection(output, target)
-        union = np.prod(output['d']) + np.prod(target['d']) - intersect
-        return intersect / union
+        return iou(output, target)
 
 
 class MeanDistanceError(GeometricMetrics):
@@ -168,4 +247,12 @@ if __name__ == '__main__':
     m.test = "nachtr"
     metrics = [m, MeanDistanceError(), IoU()]
     result = {repr(m): m(out, target) for m in metrics}
+    print(result)
+
+    metrics = [BinaryAPSklearn()]
+    result = {repr(m): m([[out[0]]], [[target[0]]]) for m in metrics}
+    print(result)
+
+    metrics = [BinaryAPSklearn()]
+    result = {repr(m): m([out], [target]) for m in metrics}
     print(result)
