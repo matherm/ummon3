fixed average function for diffrent size in the subset. (Added new class for online calculation

tests/test_metrics.py
@@ -2,7 +2,7 @@
 # @Author: Daniel Dold and Markus Käppeler
 # @Date:   2019-11-20 15:42:51
 # @Last Modified by:   Daniel
-# @Last Modified time: 2020-07-29 23:31:16
+# @Last Modified time: 2020-07-30 09:26:46
 import numpy as np
 from scipy.spatial.transform import Rotation
 from sklearn import preprocessing
@@ -326,5 +326,5 @@ class TestMetrics:
                      r=Rotation.from_euler('xyz', [45, 45, 30], degrees=True).as_matrix())]
         # test
         result = iou(box1, box2)
-        expected_res = np.mean([4 / 6, 1, 0])
+        expected_res = [4 / 6, 1, 0]
         assert np.isclose(result, expected_res).all()

ummon/metrics/geometric_metrics.py
@@ -2,7 +2,7 @@
 # @Author: Daniel Dold, Markus Käppeler
 # @Date:   2019-11-20 10:08:49
 # @Last Modified by:   Daniel
-# @Last Modified time: 2020-07-29 23:41:11
+# @Last Modified time: 2020-07-30 09:07:03
 import numpy as np
 from scipy.spatial.transform import Rotation
 from scipy.sparse import csr_matrix
@@ -323,15 +323,9 @@ class BinaryF1(ObjectDetectionMetric):
 
 
 class GeometricMetrics(OnlineMetric):
-    """ A on-line metric must return a single value. 
-        The ummon online average function requires this behavior.  
-        ->  This function calculates the mean per object and minibatch. 
-            The averaging over the minibatches is done via ummon.utils.average_utils function
-    """
-
     def __call__(self, output: list, target: list):
         results = [self.func(c1, c2) for c1, c2 in zip(output, target)]
-        return np.mean(results)
+        return results
 
     @classmethod
     def __repr__(cls):

ummon/utils/average_utils.py
@@ -1,6 +1,9 @@
 import numpy as np
 import torch
 
+
+__all__ = ['online_average', 'OnlineAverage', 'moving_average']
+
 def online_average(value, count, avg):
     # BACKWARD COMPATIBILITY FOR TORCH < 0.4
     if type(value) is not float and not isinstance(value, np.float):
@@ -10,22 +13,58 @@ def online_average(value, count, avg):
             value = value.data[0]
     navg = avg + (value - avg) / count
     return navg
-     
+
+
+class OnlineAverage():
+    """docstring for OnlineAverage
+
+    This online average function computes a running average.
+    It can handle a single value or a list. 
+    The list can also be a subpart of the complete list which should be averaged.    
+
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.n_ = 0.0
+
+    def __call__(self, value, avg):
+        if type(value) is torch.Tensor:
+            mvalue = torch.mean(value).item()
+            try:
+                items = len(value)
+            except TypeError as e:
+                items = 1
+            except Exception as e:
+                raise e
+        elif type(value) is list or type(value) is np.ndarray:
+            mvalue = np.mean(value)
+            items = len(value)
+        else:
+            mvalue = value
+            items = 1
+        self.n_ += items
+        return avg + (mvalue - avg) * items / self.n_
+
+    def reset(self):
+        self.n_ = 0
+
+
 def moving_average(t, ma, value, buffer):
     """
     Helper method for computing moving averages.
-    
+
     Arguments
     ---------
     * t (int) : The timestep
     * ma (float) : Current moving average
     * value (float) : Current value
     * buffer (List<float>) : The buffer of size N
-    
+
     Return
     ------
     * moving_average (float) : The new computed moving average.
-    
+
     """
     # BACKWARD COMPATIBILITY FOR TORCH < 0.4
     if type(value) is not float and not isinstance(value, np.float):
@@ -33,7 +72,7 @@ def moving_average(t, ma, value, buffer):
             value = value.item()
         else:
             value = value.data[0]
-    
+
     n = buffer.shape[0]
     if ma is None:
         moving_average = value